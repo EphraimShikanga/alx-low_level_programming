f6971093bb879f3be6529b9a006db67aead53d22
Ephraim182004 <ephraimshikanga@gmail.com>
2022-12-08 10:44:09 -0800

    Betty coding

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,7 +6,9 @@
  */
 int main(void)
 {
-char new_standard_error[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	char err[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
-fwrite(new_standard_error, 59, 1, stderr);
-return (1); }
+	fwrite(err, 59, 1, stderr);
+	return (1);
+}
+
