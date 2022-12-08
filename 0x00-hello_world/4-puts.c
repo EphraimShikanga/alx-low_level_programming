9f50f7410b35033aff817e9a8acfd4ff2b7147f8
Ephraim182004 <ephraimshikanga@gmail.com>
2022-12-08 09:11:57 -0800

    Betty coding

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * @puts() - used to write a string to a file output
+ * Return: If success always 0
+ */
+
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle");
+	return (0);
+}
+
