07f8dfbd88aaa441bdb61beede7f9857814af677
Ephraim182004 <ephraimshikanga@gmail.com>
2022-12-08 10:15:34 -0800

    Betty codding

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,5 +12,6 @@ int main(void)
 	printf("size of an int: %ld byte(s)\n", sizeof(int));
 	printf("size of a long int: %li byte(s)\n", sizeof(long int));
 	printf("size of a long long int: %li byte(s)\n", sizeof(long long int));
+	printf("size of a float: %ld byte(s)\n", sizeof(float));
 	return (0);
 }
