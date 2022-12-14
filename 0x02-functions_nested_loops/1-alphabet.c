fcbb752ecebb16c3b8461a423694f52c596bd1a6
Ephraim182004 <ephraimshikanga@gmail.com>
2022-12-14 09:33:17 -0800

    alphabet putchar

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_alphabet-Entry point
+ * Decsription:prints the alphabet in lower case followed by a new line
+ * Return: void
+ */
+
+void print_alphabet(void)
+{
+	char x;
+
+	do {
+		_putchar(x)
+	} while (x = 'a', x <= 'z', x++);
+	{
+	_putchar('\n')
+	}
+
+}
