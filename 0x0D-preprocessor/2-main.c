00d930ee2a85e7f1d417724f118be76542e0a215
Ephraim182004 <ephraimshikanga@gmail.com>
2023-03-30 06:54:46 +0300

    Cstuff

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 /**
- *main - Prints the name of the file
- *Return: 0
+ * main - entry point
+ * Return: 0 Always success
  */
 int main(void)
 {
-printf("%s\n", __FILE__);
-return (0);
+	printf("%s\n", __FILE__);
+	return (0);
 }
+
--- 0x0D-preprocessor/README.md
+++ 0x0D-preprocessor/README.md
@@ -1,1 +1,1 @@
-This contains Preprocessor files
+preprocessor
